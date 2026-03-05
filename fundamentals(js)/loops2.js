d61d5924d3c26583e10df244c4d18670285f75d2
Rohithsilent <shailajapullola@gmail.com>
2026-03-05 19:40:38 +0530

    Add initial JavaScript files for basic functions, loops, and array operations

---
+++ fundamentals(js)/loops2.js
@@ -0,0 +1,46 @@
+//write a function that searches for an element in an array and returns the index, if the element is not present then just return -1.
+
+
+function find(arr,element) {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] == element) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let element = 7;
+
+let index = find(arr, element);
+console.log(index);
+
+
+//write a function that returns the number of negative numbers in an array
+
+function negative(arr) {
+    let countn = 0;
+    let countz = 0;
+    let countp = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] < 0) {
+            countn++;
+        }
+        else if (arr[i] == 0) {
+            countz++;
+        }
+        else {
+            countp++;
+        }
+    }
+    return { countn, countp, countz };
+}
+
+let arr1 = [-1, 3, 0, -4, 5, 0, -4, 6];
+let { countn: neg, countp: positive, countz: zero } = negative(arr1);
+
+console.log("negative" + neg);
+console.log("positive" + positive);
+console.log("zero" + zero);
+
