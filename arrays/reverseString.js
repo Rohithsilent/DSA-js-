1bcb841be9fd3fc6c9edaca20aa9464612efbdc8
Rohithsilent <shailajapullola@gmail.com>
2026-03-16 19:35:49 +0530

    reverse string

---
+++ arrays/reverseString.js
@@ -0,0 +1,13 @@
+var reverseString = function(s){
+    let n = s.length;
+
+    for(let i = 0;i<n/2;i++){
+        let temp = s[i];
+        s[i] = s[n - (i+1)];
+        s[n - (i+1)] = temp;
+    }
+
+    return s;
+}
+
+console.log(reverseString(["h","e","l","l","o"])); 
