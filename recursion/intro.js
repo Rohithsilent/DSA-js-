180a8fa6c16ded6aa6044c5bb8e5dd16d2e8af71
Rohithsilent <shailajapullola@gmail.com>
2026-08-14 15:17:54 +0530

    recursion intro

---
+++ recursion/intro.js
@@ -0,0 +1,37 @@
+function func(num){
+    if(num == 0) return ;
+    
+     console.log(num);
+     num -= 1;
+     func(num);
+    
+    
+}
+
+function print(n){
+    
+    if(n>num) return ;    if(n>num) return ;
+
+
+    console.log(n);
+    n += 1;
+    print(n);
+}
+
+function sum(n){
+    if(n == 0) return 0;
+
+    return n + sum(n-1);
+}
+
+function sum_arr(arr){
+    
+}
+
+num = 9
+func(num)
+
+print(1)
+
+
+console.log(sum(5))
